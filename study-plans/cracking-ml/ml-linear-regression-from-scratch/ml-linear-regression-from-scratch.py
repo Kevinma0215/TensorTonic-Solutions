4e6825f6a6d31e853561d605bae193f7e97000a8
Chi-Lung, Kevin, Ma <75787589+Kevinma0215@users.noreply.github.com>
2026-04-12 15:07:32 -0700

    Update solution for ml-linear-regression-from-scratch

--- study-plans/cracking-ml/ml-linear-regression-from-scratch/ml-linear-regression-from-scratch.py
+++ study-plans/cracking-ml/ml-linear-regression-from-scratch/ml-linear-regression-from-scratch.py
@@ -4,22 +4,30 @@ def linear_regression(X, y, lr, epochs):
     """
     Returns: tuple (weights, bias)
     """
-    
-    X = np.asarray(X)
-    d = X.shape[1]
-    n = X.shape[0]
+
+    # Use np.array to copy list to np array
+    X = np.array(X, dtype=float) 
+    y = np.array(y, dtype=float)
+
+    # shape can be use as item
+    n, d = X.shape
     
     weights = np.zeros(d)
     bias = 0.0
 
     for _ in range(epochs):
+        # Notice inner product use @
         y_hat = X @ weights + bias
         y_diff = y_hat - y
 
-        dw = (2 / n) * X.T @ (y_diff)
+        dw = (2 / n) * (X.T @ y_diff)
         db = (2 / n) * np.sum(y_diff)
         
         weights -= lr * dw
         bias -= lr * db
+
+    # Remember to round every time before return
+    weights = np.round(weights, decimals=4)
+    bias = round(bias, 4)
 
     return weights, bias
